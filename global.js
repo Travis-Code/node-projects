117fdb6f0cfccc8b67e55bf14317ef808a54a14e
Trav <jorkilla@gmail.com>
2016-03-17 18:43:31 -1000

    NODE
    understanding node

---
+++ global.js
@@ -0,0 +1,13 @@
+var path = require("path");
+
+console.log(`Rock on World from ${path.basename(__filename)}`);
+
+console.log(process.argv);
+
+var hello = "YA MAMA";
+console.log(hello);
+
+/*console.log(__dirname);
+
+console.log(__filename);
+*/
